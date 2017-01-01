0b1d102bb0cc7eac133e5119f6fdcee89bbbc012
sky <312wwj@163.com>
2017-01-01 20:19:32 +0800

    add order by created_at to group's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
 
   def new
-    @group = Group.find(params[:group_id])
+    @group = Group.find(params[ :group_id])
     @post = Post.new
   end
 
